5cd83bf1e62482ed1659d4e40be71fa843c1e708
HienVDRK <hienvd@rikkeisoft.com>
2020-02-20 17:31:05 +0700

    thay doi theo comment review

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+
+function Footer() {
+    let css_footer = {height: "100px", lineHeight: "25px", background: "gainsboro", borderRadius: 5, textAlign: "center", paddingTop: "30px"}
+    return <div style={{ marginTop: "50px"}}>
+        <h4 style={css_footer}>
+            Đề Tài 3 - Tìm kiếm phim trên trang IMDB <br/>
+                Copyright: "(c) 2020 Rikkeisoft"
+       </h4> 
+    </div>
+}
+export default Footer
